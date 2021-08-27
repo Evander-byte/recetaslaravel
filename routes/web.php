830948f535e4b5a99db8f782857a242ef5bc7fc5
Evander-byte <emondragon571@gmail.com>
2021-08-26 20:31:20 -0500

    Vistas y acciones para Perfiles

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ Route::get('/recetas/{receta}/edit', 'RecetaController@edit')->name('recetas.edi
 Route::put('/recetas/{receta}', 'RecetaController@update')->name('recetas.update');
 Route::delete('/recetas/{receta}', 'RecetaController@destroy')->name('recetas.destroy');
 
+Route::get('/perfiles/{perfil}', 'PerfilController@show')->name('perfiles.show');
+Route::get('/perfiles/{perfil}/edit', 'PerfilController@edit')->name('perfiles.edit');
+
 
 Auth::routes();
 
