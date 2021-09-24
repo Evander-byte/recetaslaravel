1e923c638516e4692ce33af489ad395fe61724c4
Evander-byte <emondragon571@gmail.com>
2021-09-23 19:03:50 -0500

    Mostrar recetas por likes

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Receta;
+use App\CategoriaReceta;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
@@ -10,9 +12,19 @@ class InicioController extends Controller
     //
     public function index ()
     {
+        // Mostrar las recetas por votos
+        // $votadas = Receta::has('likes', '>', 1)->get(); ESTATICO
+        $votadas = Receta::withCount('likes')->ordeBy('likes_count', 'desc')->take(3)->get(); // DINAMICO
         // Obtener las recetas más nuevas
         $nuevas = Receta::latest()->take(6)->get();
 
-        return view('inicio.index', compact('nuevas'));
+        // Recetas por categorias
+        $categorias = CategoriaReceta::all();
+        // Agrupar las reectas por categoria
+        $recetas = [];
+        foreach($categorias as $categoria){
+            $recetas[Str::slug($categoria->nombre)][] = Receta::where('categoria_id', $categoria->id)->take(3)->get();
+        }
+        return view('inicio.index', compact('nuevas', 'recetas', 'votadas'));
     }
 }
---
+++ resources/views/ui/receta.blade.php
@@ -0,0 +1,27 @@
+<div class="col-md-4">
+    <div class="card shadow">
+        <img class="card-img-top" src="/storage/{{ $receta->imagen }}" alt="Imagen de la receta">
+        <div class="card-body">
+            <h3 class="card-title">{{ $receta->titulo }}</h3>
+
+            <div class="meta-receta d-flex justify-content-between">
+                @php
+                    $fecha = $receta->created_at
+                @endphp
+                <p class="text-primary fecha font-weight-bold">
+                    <fecha-receta fecha="{{$fecha}}"></fecha-receta>
+                </p>
+                <p>
+                    {{ count($receta->likes) }} Les gustó
+                </p>
+            </div>
+            <p class="card-text">
+                {{ Str::words(strip_tags($nueva->preparacion), 20, ' ...') }}
+            </p>
+            <a 
+                href="{{ route('recetas.show', ['receta' => $receta->id]) }}"
+                class="btn btn-primary d-block btn-receta"
+            >Ver receta</a>
+        </div>
+    </div>
+</div>
